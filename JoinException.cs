481342ddb7a8928b6517901f256b09d26d11003f
qbakernc <79540225+qbakernc@users.noreply.github.com>
2021-02-24 20:44:58 -0500

    Add files via upload

---
+++ JoinException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace AisleSwitcherLibrary
+{
+    internal class JoinException : Exception
+    {
+        public JoinException()
+            : base("The called join number located in the InputJoin array can not be equal to 0.")
+        {
+        }
+    }
+}
